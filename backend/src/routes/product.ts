acdb2956bbca8b4c12929146b1d3a96cd04be12c
Eric Donnelly <200433532@student.georgianc.on.ca>
2024-05-20 11:24:09 -0400

    Posting Works

--- backend/src/routes/product.ts
+++ backend/src/routes/product.ts
@@ -3,7 +3,7 @@ import { prisma } from "../../prisma/db.setup";
 const productRouter = Router();
 
 /* GET ALL. */
-productRouter.get("/", async function (req, res, next) {
+productRouter.get("/", async function (_req, res) {
   const allProducts = await prisma.product.findMany();
 
   return res.status(200).send(allProducts);
@@ -11,7 +11,7 @@ productRouter.get("/", async function (req, res, next) {
 
 // GET ONE  - ID // ANY UNIQUE VALUE
 
-productRouter.get("/:id", async function (req, res, next) {
+productRouter.get("/:id", async function (req, res) {
   const idAsNum = +req.params.id;
 
   if (isNaN(idAsNum))
@@ -31,6 +31,46 @@ productRouter.get("/:id", async function (req, res, next) {
 });
 
 // POST - THIS NEEDS TO BE AUTHENTICATED WE DON"T WANT RANDOMS TO BE ABLE TO ADD THERE OWN PRODUCTS
+
+// TODO: ADD MIDDLEWARES TO :  VALIDATE USER MAKING REQUEST,VALIDATE REQUEST BODY
+
+productRouter.post("/create", async function (req, res) {
+  const {
+    id,
+    title,
+    inStock,
+    category,
+    brand,
+    packageSize,
+    price,
+    description,
+    image,
+  } = req.body;
+  const newProduct = await prisma.product.create({
+    data: {
+      id,
+      title,
+      inStock,
+      casePrice: price,
+      package: packageSize,
+      Brands: {
+        connect: { id: brand },
+      },
+      Categories: { connect: { id: category } },
+      effects: { connect: [] },
+      ColorStrings: { connect: [] },
+      description,
+      image,
+    },
+  });
+
+  if (!newProduct)
+    return res
+      .status(500)
+      .send({ message: "Internal Server Error. Product not Created" });
+
+  return res.status(201).send(newProduct);
+});
 
 // PATCH (or PUT) - THIS NEEDS TO BE AUTHENTICATED WE DON"T WANT RANDOMS TO BE ABLE TO EDIT THE PRODUCTS   -
 
