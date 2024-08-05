1abb26882603eac19f57128277e204eaed980d5a
Eric Donnelly <200433532@student.georgianc.on.ca>
2024-08-05 15:55:29 -0400

    couple minor changes

---
+++ prisma/migrations/20240801221457_added_brands_and_effects/migration.sql
@@ -0,0 +1,21 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "Brand" ADD VALUE 'PYRO_PIRATE';
+ALTER TYPE "Brand" ADD VALUE 'IRONMAN';
+
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "Effects" ADD VALUE 'CONFETTI';
+ALTER TYPE "Effects" ADD VALUE 'SNAKE';
