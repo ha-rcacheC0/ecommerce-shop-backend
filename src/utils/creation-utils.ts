b08166ef0d7cabc17197bc437fbc87304180df51
Eric Donnelly <eric@donnellydev.ca>
2025-05-13 04:03:48 -0400

    minor updates for codyb launch event

--- src/utils/creation-utils.ts
+++ src/utils/creation-utils.ts
@@ -1,3 +1,3 @@
 export function calcUnitPrice(casePrice: number, qty: number): number {
-  return Math.ceil((casePrice / 2.016 / qty) * 2.5) - 0.01;
+  return Math.ceil((casePrice / 2.016 / qty) * 2.65) - 0.01;
 }
